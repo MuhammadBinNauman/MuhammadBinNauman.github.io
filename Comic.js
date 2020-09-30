5be459e6675958bae1ee6934c08e89e3ec60b548
MuhammadBinNauman <57341182+MuhammadBinNauman@users.noreply.github.com>
2020-10-01 03:58:33 +0400

    added comments to all

--- Comic.js
+++ Comic.js
@@ -1,23 +1,21 @@
 let x = document.getElementById('placeholder');
-let y = document.getElementById('myarrow');
-//x.style.display ='block';
-//y.style.display ='none';
+let y = document.getElementById('myarrow');         // connecting the elements id to the variables
 
 let ShowComic = () => {
-    x.style.display ='block';
+    x.style.display ='block';                       // making the comic placeholder images hidden when the page is visited and on clicking the arrow the placeholder images are shown
     y.style.display ='none';
 
 }
 
 
-y.addEventListener('click', ShowComic)
+y.addEventListener('click', ShowComic)              // adding the mouse click trigger to enable the above to happen
 
 let a = document.getElementById('placeholder2');
 let b = document.getElementById('myarrow2');
 
 
 let ShowComic2 = () => {
-    a.style.display ='block';
+    a.style.display ='block';                       // the same as above but for the ongoing comics
     b.style.display ='none';
 
 }
